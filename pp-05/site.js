bb8c3a9a6fec00581839446f27c5e5825ddceabb
TeddyFrancis <31176229+TeddyFrancis@users.noreply.github.com>
2017-11-12 19:00:01 -0600

    Hide audio by removal from DOM with JavaScript active.

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,4 +1,16 @@
-function ringDoorbell() {
+//  function ringDoorbell() {
+//   var doorbell = new Audio('media/doorbell.mp3');
+//   doorbell.play();
+//  }
+
+//  Release the dollar-sign from jquery
+$.noConflict();
+
+//  Re-introduce dollar-sign scoped to self-ex function
+(function($) {
   var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+
+  $('html').removeClass('nojs').addClass('js');
+  $("audio").remove();
+
+})(jQuery);
